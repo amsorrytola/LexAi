160e8ea1f8e87e12240ba6f9cec77f0adefb2bf0
amsorrytola <talhaansariitr@gmail.com>
2025-07-07 23:47:24 +0530

    chore: initialize project with clean structure

--- src/LexAi_backend/src/lib.rs
+++ src/LexAi_backend/src/lib.rs
@@ -1,32 +1,22 @@
+// lib.rs
 
-mod api;
-mod logic;
-mod state;
-mod types;
+use candid::Principal;
 
-use ic_cdk_macros::*;
+#[ic_cdk::query]
+fn whoami() -> Principal {
+    ic_cdk::caller()
+}
 
-// 👇 EXPORT the functions from api.rs
-pub use api::{
-    submit_question,
-    get_all_questions,
-    answer_question,
-};
-
-#[init]
-fn init() {}
-
-#[post_upgrade]
-fn post_upgrade() {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::write;
     use candid::export_service;
 
     #[test]
     fn generate_did() {
         export_service!();
-        std::fs::write("LexAi_backend.did", __export_service()).unwrap();
+        write("LexAi_backend.did", __export_service()).expect("Failed to write .did file");
     }
 }
